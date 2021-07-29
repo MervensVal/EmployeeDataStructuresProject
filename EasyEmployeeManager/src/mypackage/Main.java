e81467796d82c22c99d75923237324fe6d87ea1b
Mervens Val <valmervens@gmail.com>
2021-07-29 18:11:59 -0400

    Added more textfileDB functionality and fixed some bugs

--- EasyEmployeeManager/src/mypackage/Main.java
+++ EasyEmployeeManager/src/mypackage/Main.java
@@ -9,14 +9,61 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to our non DB Employee management program!");
-		Main mainObj = new Main();
-		mainObj.insertData();
-		mainObj.displayEmployees();
+		
+			System.out.println("Choose an option from the list below");
+			
+			System.out.println("*******************************************");
+			System.out.println("(0)	Quit");
+			System.out.println("(1)	Display All Employee Information");
+			System.out.println("(2)	Insert a New Employee");
+			System.out.println("(3)	Update Employee Information");
+			System.out.println("(4)	View Employee Information by ID (Hash Table & Linked List Implementation)");
+			System.out.println("********************************************");
+			
+			
+			chooseUserChoices();
+		
 	}
 	
+	//--------------------------------------------------------------------------------------------------------
+		public static boolean chooseUserChoices(/*Employee[] countyArray*/) 
+		{
+			Main mainObj = new Main();			
+			Scanner input = new Scanner(System.in);
+			System.out.println("Please make a selection");
+			String userDecision = input.next();
+			System.out.println();
+			
+			switch(userDecision)
+			{
+			case "0":
+				System.out.println("Program ended. Thank you");
+				return true;
+			case "1":
+				mainObj.displayEmployees();
+				System.out.println("All employee information displayed");
+				break;
+			case "2":
+				mainObj.insertData();
+				System.out.println("New employees inserted");
+				break;
+			case "3":
+				mainObj.update();
+				System.out.println("Employee information has been updated");
+				break;
+			case "4":
+				System.out.println("Selected employee information displayed");
+				break;
+			default:
+				System.out.println("Incorrect information entered");
+				}
+			return false;
+		}
+	//----------------------------------------------------------------------------------------------------------------------------------------
 	public void displayEmployees() 
 	{
-		System.out.printf("%-25s%-25s%-25s%-25s%-25s%-25s","ID " ," First Name  " , " Last Name " , " Department " , " Position " , " Salary");
+		System.out.println();
+		System.out.printf("%-25s%-25s%-25s%-25s%-25s%-25s%-25s","ID " ," First Name  " , " Last Name " , " Email Address " , " Position " , " Location", "Current Employee");
 		System.out.println();
 		try 
 		{
@@ -24,21 +71,26 @@ public class Main {
 			String s = "";
 			while ((s = br.readLine()) !=null) 
 			{
-				String data[] = new String[5];
+				String data[] = new String[7];
 				data = s.split(",");
-				for(int i = 0; i < 5; i++) {
+				for(int i = 0; i < 7; i++) {
 					System.out.printf("%-25s", data[i]);
 				}
 				System.out.println();
 			}
 		}catch(Exception e) 
 		{
+			
 		}
 		
 	}
 	
+	//----------------------------------------------------------------------------------------------------------------------------------------
+	
 	public void insertData() 
 	{
+		System.out.println();
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Employee Id: ");
 		String epmployeeID = sc.nextLine();
@@ -49,25 +101,137 @@ public class Main {
 		System.out.println("Enter Employee Last Name: ");
 		String epmployeeLN = sc.nextLine();
 		
-		System.out.println("Enter Employee Department: ");
-		String epmployeeDepartment = sc.nextLine();
+		System.out.println("Enter email: ");
+		String email = sc.nextLine();
 		
 		System.out.println("Enter Employee Position: ");
 		String epmployeePosition = sc.nextLine();
 		
-		System.out.println("Enter Employee Salary: ");
-		String epmployeeSalary = sc.nextLine();
+		System.out.println("Enter Location: ");
+		String epmployeeLocation = sc.nextLine();
 		
+		System.out.println("Current Employee? (Y/N): ");
+		String employementStatus = sc.nextLine();
+		
+		System.out.println();
 		try 
 		{
 			File fileObj = new File(employeeDbFile);
 			PrintWriter pw = new PrintWriter(new FileOutputStream(fileObj,true));
-			pw.append(epmployeeID + "," + epmployeeFN + "," +epmployeeLN + "," + epmployeeDepartment+ "," +epmployeePosition + "," + epmployeeSalary + "\n");
+			
+			pw.append(epmployeeID + "," + epmployeeFN + "," +epmployeeLN + "," + email+ "," +epmployeePosition + "," + epmployeeLocation + "," + employementStatus +"\n");
 			pw.close();
 		}catch(Exception e) 
 		{
 			
 		}
 	}
+	
+	//----------------------------------------------------------------------------------------------------------------------------------------
+
+	public void update() 
+	{
+		//Part 1)
+		System.out.println();
+		System.out.println("Enter the ID of the employee you want to update: ");
+		Scanner sc = new Scanner(System.in);
+		int idToEmployeeToChange = sc.nextInt();
+		String ColumnsToChange[] = {"First Name:  " , "Last Name: " , "Email" , "Position: ","Location: " , "Employment Status: "};
+		char yesNo[] = new char[6];
+		String UpdatedInfo[] = new String[6];
+		
+		System.out.println("Select the columns you want to update. Type (Y:Yes | N:No)"); 
+		for(int i = 0; i < 6; i++) 
+		{
+			//asks for which columns to change
+			System.out.println(ColumnsToChange[i] + "");
+			//accepts the users answer
+			char temp = sc.next().charAt(0);
+			//adds the answer to this array
+			yesNo[i] = temp;
+		}
+
+		
+		//Part 2)
+		System.out.println();
+		System.out.println("----------------------------------------"); 
+		System.out.println("Add new values for the user"); 
+		for(int i = 0; i < 6; i++) 
+		{
+			System.out.println(ColumnsToChange[i]); 
+			if (yesNo[i] == 'Y' || yesNo[i] == 'y') 
+			{
+				//add user input to array to update values
+				Scanner sc1 = new Scanner(System.in);
+				String tempVal = sc1.nextLine();
+				UpdatedInfo[i] = tempVal;
+				System.out.println(); 
+			}else
+			{
+				System.out.println("Will not change"); 
+			}
+		}
+		System.out.println();
+		//Part 3)
+		StringBuffer sb = new StringBuffer();
+		try 
+		{
+			BufferedReader br = new BufferedReader(new FileReader(employeeDbFile));
+			String s="";
+			while((s=br.readLine())!=null) //read line of text
+			{
+				String data[] = new String[6];
+				data = s.split(","); //split text based off of comma
+				if(idToEmployeeToChange == Integer.parseInt(data[0])) //if id matches
+				{
+					String row = data[0] + ",";
+					for(int i = 0; i < 6; i++) 
+					{
+						if(yesNo[i] == 'Y' || yesNo[i] == 'y') 
+						{
+							row = row + UpdatedInfo[i] + ",";
+						}
+						else 
+						{
+							row = row + data[i+1] + ",";
+						}
+					}
+					sb.append(row);
+					sb.append("\n");
+				}
+				else 
+				{
+					sb.append(s);
+					sb.append("\n");
+				}
+			}
+			
+			//Part 4)
+		File fileObj = new File(employeeDbFile);
+		PrintWriter pw = new PrintWriter(new FileOutputStream(fileObj,false));
+		pw.print(sb.toString());
+		pw.close();
+		}
+		catch(Exception e) 
+		{
+			
+		}
+
+
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
